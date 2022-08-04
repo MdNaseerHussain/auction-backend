e43f9a8cd0744b40f385a2e33011d98226b1914f
MdNaseerHussain <mdnajju013@gmail.com>
2022-08-04 12:59:12 +0530

    Boiler Plate

---
+++ src/test/java/com/auction/live/LiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
