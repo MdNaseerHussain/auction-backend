8b54f7b9a75f463c589aa49dbeccb9f93e159153
MdNaseerHussain <mdnajju013@gmail.com>
2022-08-04 23:02:36 +0530

    removed cross origin

--- src/main/java/com/auction/live/controllers/Hello.java
+++ src/main/java/com/auction/live/controllers/Hello.java
@@ -1,11 +1,11 @@
 package com.auction.live.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
+// @CrossOrigin
 public class Hello {
 
     @GetMapping("/")
