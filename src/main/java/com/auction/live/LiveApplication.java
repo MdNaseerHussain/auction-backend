ea47ec16f2c6b06594d1febd9b75ed7122119fb1
MdNaseerHussain <mdnajju013@gmail.com>
2022-08-26 02:21:36 +0530

    crossorigin

--- src/main/java/com/auction/live/LiveApplication.java
+++ src/main/java/com/auction/live/LiveApplication.java
@@ -3,9 +3,11 @@ package com.auction.live;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableScheduling
+@CrossOrigin(origins = "http://localhost:3000")
 public class LiveApplication {
 
 	public static void main(String[] args) {
