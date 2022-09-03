86e99b4ee373768656641f230e4c9bf5c6cc1339
ceasle <sujithrockzzzz36@gmail.com>
2022-09-03 11:57:21 +0530

    empty commit

--- src/main/java/com/auction/live/security/SecurityConfiguration.java
+++ src/main/java/com/auction/live/security/SecurityConfiguration.java
@@ -49,8 +49,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/register")
                 .permitAll()
-                .antMatchers("/auction")
-                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
